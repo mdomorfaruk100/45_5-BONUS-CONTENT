4cd5fb7b66d0a7697c7d59188f9fe0db22b9073c
mdomorfaruk100 <mdomorfarukdeveloper988@gmail.com>
2025-12-02 09:12:41 +0600

    Update firebseConfig.js

--- src/context/firebseConfig.js
+++ src/context/firebseConfig.js
@@ -1,11 +1,5 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyCJ-aCBJaodJeQ4wDbtFvBBxfWw_Eub958",
-    authDomain: "hot-onion-40ef8.firebaseapp.com",
-    projectId: "hot-onion-40ef8",
-    storageBucket: "hot-onion-40ef8.firebasestorage.app",
-    messagingSenderId: "420737204188",
-    appId: "1:420737204188:web:9d73d60d03a2363fee9919",
-    measurementId: "G-GS8J9GGN0T"
+ 
 };
 
 export default firebaseConfig;
